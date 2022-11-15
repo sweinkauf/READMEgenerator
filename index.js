95fc7a7886aa720e94a3abdc70dd69220c09f85b
sweinkauf <scott.weinkauf29@gmail.com>
2022-11-15 00:21:21 -0600

    added inquirer and prompts

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is the title of your project?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Enter description of your project',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'Enter installation instructions',
+      name: 'install',
+    },
+    {
+      type: 'input',
+      message: 'Enter usage information',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message: 'Enter contribution guidelines',
+      name: 'contribution',
+    },
+    {
+      type: 'input',
+      message: 'Enter test instructions',
+      name: 'test',
+    },
+  ])
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
